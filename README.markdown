6aa54d896e5f33310ae6e34c29f7f6b0a9dadd8e
Sam Beam <sbeam@onsetcorps.net>
2010-11-23 23:27:08 -0500

    readme, license, cruftectomy

--- README.markdown
+++
@@ -1,3 +0,0 @@
-= Customer Returns
-
-Description goes here
--- public/javascripts/customer_returns.js
+++
@@ -1,8 +0,0 @@
-
-$( function() {
-
-            $('#order_details td.rma_check input').bind('click', function() {
-                if (this.checked) $(this).parents('tr').next().find('div.fields').slideDown('fast');
-                else $(this).parents('tr').next().find('div.fields').slideUp('fast');
-            });
-} );
